bc444efa24c39d96da8dcdac71272b9411b6cf51
Hwanak Hong <ghdghkwo5@gmail.com>
2025-04-29 18:51:57 +0900

    11111

--- ConsoleApp1/ConsoleApp1/20250428/Test1.cs
+++ ConsoleApp1/ConsoleApp1/20250428/Test1.cs
@@ -8,5 +8,32 @@ namespace ConsoleApp1._20250428
 {
     public class Test1
     {
+        //문제 1 정수형 변수 a, 실수형 변수 b 두 변수를 선언하고 초기화한 뒤, 두 수의 합을 출력하시오.
+        public void Question1() {
+            int a = 1;
+            double b = 3.3;
+
+            Console.Write($"{a}+{b} = {a+b}");
+        }
+        //문제 2
+        public void Question1()
+        {
+            int a = 1;
+            double b = 3.3;
+
+            Console.Write($"{a}+{b} = {a + b}");
+        }
+
+        //문제 3
+
+        //문제 4
+
+        //문제 5
+
+        //문제 6
+
+        //문제 7
+
+        //문제 8
     }
 }
