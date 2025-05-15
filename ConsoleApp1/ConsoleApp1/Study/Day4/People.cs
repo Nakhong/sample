f2fa461000d5cd1a01d9c543c2d26729669b87c6
hhn <hhn@HHN-PC>
2025-05-15 17:26:39 +0900

    ㅋㅌㅊㅍ

---
+++ ConsoleApp1/ConsoleApp1/Study/Day4/People.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Study.Day4
+{
+    public class People
+    {
+        string name; //이름
+        string tel; // 전화번호
+        int age; //나이
+        string rank; // 직급
+        string position; //사업본부
+
+    }
+}
