cdef689527af08627a81022f7653ffcb841f8d7c
Hwanak Hong <ghdghkwo5@gmail.com>
2025-05-26 20:57:04 +0900

    zz

--- ConsoleApp1/ConsoleApp1/Study/Day6/Model/Employee.cs
+++ ConsoleApp1/ConsoleApp1/Study/Day6/Model/Employee.cs
@@ -8,26 +8,33 @@ namespace ConsoleApp1.Study.Day6.Model
 {
     public class Employee : Common
     {
+        public string Age { get; set; }
         public string Email { get; set; }
         public string Contact { get; set; }
+        public string Gender { get; set; }
         public string EmployeePosition { get; set; }
         public Department Department { get; set; }
 
-        public Employee(string name, string email, string contact, Department department, string position)
+        public Employee(string name, string age, string gender, string position, string contact, string email,  Department department)
         {
             Name = name;
-            Email = email;
-            Contact = contact;
-            Department = department;
+            Age = age;
+            Gender = gender;
             EmployeePosition = position;
+            Contact = contact;
+            Email = email;
+            Department = department;
         }
 
         public void PrintInfo()
         {
             Console.WriteLine("직원 정보");
             Console.WriteLine($"이름: {Name}");
-            Console.WriteLine($"이메일: {Email}");
+            Console.WriteLine($"나이: {Age}");
+            Console.WriteLine($"성별: {Gender}");
+            Console.WriteLine($"직책: {EmployeePosition}");
             Console.WriteLine($"연락처: {Contact}");
+            Console.WriteLine($"이메일: {Email}");
             Console.WriteLine($"소속 부서: {Department.Name}");
             Console.WriteLine($"소속 회사: {Department.Company.Name}");
             Console.WriteLine();
--- ConsoleApp1/ConsoleApp1/Study/Day6/Service/CreateTree.cs
+++ ConsoleApp1/ConsoleApp1/Study/Day6/Service/CreateTree.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleApp1.Study.Day5;
+﻿
+using ConsoleApp1.Study.Day6.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,22 +12,74 @@ namespace ConsoleApp1.Study.Day6.Service
     {
         public Dictionary<string, string> Data { get; set; }
 
+        //public static void test(string row)
+        //{
+        //    string[] parts = row.Split(';');
+
+        //    Company company = new Company(parts[0], parts[1], parts[2]);
+
+        //    Department department = new Department(parts[3], company);
+        //    company.Departments.Add(department);
+
+        //    Employee employee = new Employee(parts[4], parts[5], parts[6], department, parts[7]);
+        //    department.Employees.Add(employee);
+
+        //    company.PrintInfo();
+        //    department.PrintInfo();
+        //    employee.PrintInfo();
+
+        //}
+
         public static void test(string row)
         {
-            string[] parts = row.Split(';');
+            string[] majorSections = row.Split('/');
+            
+            string[] companyDatas = majorSections[0].Split(';');
+            //필드에 들어갈 값이 3개
+            if (companyDatas.Length < 3)
+            {
+                Console.WriteLine("회사이름;전화번호;주소;로 입력해주세요.");
+                return;
+            }
+            Company company = new Company(companyDatas[0], companyDatas[2], companyDatas[1]);
 
-            Company company = new Company(parts[0], parts[1], parts[2]);
+            string departmentName = majorSections[1].TrimEnd(';');
 
-            Department department = new Department(parts[3], company);
+            Department department = new Department(departmentName, company);
             company.Departments.Add(department);
+            
+            string allEmployeesRaw = majorSections[2].TrimStart(',').TrimEnd(';');
+            
+            string[] employeeRecords = allEmployeesRaw.Split(',');
 
-            Employee employee = new Employee(parts[4], parts[5], parts[6], department, parts[7]);
-            department.Employees.Add(employee);
+            foreach (string empRecord in employeeRecords)
+            {
+                string[] empFields = empRecord.Split(';');
+                
+                if (empFields.Length == 7)
+                {
+                    string empName = empFields[0];
+                    string empAge = empFields[1];
+                    string empGender = empFields[2];
+                    string empPosition = empFields[3];
+                    string empContact = empFields[4];
+                    string empEmail = empFields[5];
+
+                    Employee employee = new Employee(empName, empAge, empGender, empPosition, empContact, empEmail, department);
+                    department.Employees.Add(employee);
+                }
+                else
+                {
+                    Console.WriteLine($"작성한 부분의 값이 올바르지 않아서 에러가 났습니다.");
+                }
+            }
 
             company.PrintInfo();
             department.PrintInfo();
-            employee.PrintInfo();
-
+            foreach (var emp in department.Employees)
+            {
+                emp.PrintInfo();
+            }
         }
     }
 }
